73a3e88a5b7532d7f54971ce11dee21bca54bea7
nikhilmrp <enterprises.mrp25@gmail.com>
2026-01-11 17:06:45 +0530

    bank details added

--- src/app/(admin-pages)/(configrations)/vendor-category/page.tsx
+++ src/app/(admin-pages)/(configrations)/vendor-category/page.tsx
@@ -2,13 +2,13 @@ import AddVendorModal from "@/components/ui/modal/AddVendorModal";
 import BasicTable from "@/components/ui/table/BasicTable";
 
 export default function Page() {
-    return (
-        <div>
-            <div className="flex justify-end mb-4">
-                <AddVendorModal />
-            </div>
+  return (
+    <div>
+      <div className="flex justify-end mb-4">
+        <AddVendorModal />
+      </div>
 
-            <BasicTable />
-        </div>
-    );
+      <BasicTable />
+    </div>
+  );
 }

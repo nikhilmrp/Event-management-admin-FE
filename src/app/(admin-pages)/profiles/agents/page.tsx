bc7f2ad257a7b34e6c0041d17da60ccacf993a2b
nikhilmrp <enterprises.mrp25@gmail.com>
2026-07-04 13:03:14 +0530

    Feat/react query implemented (#17)
    * react query implemented
    
    * vendor and agent profile list created

--- src/app/(admin-pages)/profiles/agents/page.tsx
+++ src/app/(admin-pages)/profiles/agents/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 import BasicTable from "@/components/ui/table/BasicTable";
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { AGENT_PROFILE_COLUMNS } from "@/components/columns/profiles";
+import { getProfileDetailsByRole } from "@/lib/api/services/profiles/general";
 
 function page() {
+  const { data: agents = [], isLoading } = useQuery({
+    queryKey: ["profiles", "agent"],
+    queryFn: async () => (await getProfileDetailsByRole("agent")).data,
+  });
+
   return (
     <>
-      <BasicTable />
+      <BasicTable columns={AGENT_PROFILE_COLUMNS} data={agents} loading={isLoading} />
     </>
   );
 }
